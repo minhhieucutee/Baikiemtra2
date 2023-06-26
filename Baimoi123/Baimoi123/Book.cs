54281f9749662fd5f96f98f3642ea936dfe090ed
Hoàng Minh Hiếu <111435941+minhhieucutee@users.noreply.github.com>
2023-06-26 01:39:54 -0700

    Add files via upload

---
+++ Baimoi123/Baimoi123/Book.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System;
+
+namespace Baimoi123
+
+{
+    class Book : Product
+    {
+        public Book(string productId, string name, double price, string producer)
+            : base(productId, name, price, producer)
+        {
+
+        }
+
+        public double Price { get; private set; }
+
+        public override double ComputeTax()
+        {
+            return Price * 0.08;
+        }
+    }
+}
---
+++ Baimoi123/Baimoi123/MobilePhone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Baimoi123
+{
+    class MobilePhone : Product
+    {
+        public MobilePhone(string productId, string name, double price, string producer)
+            : base(productId, name, price, producer)
+        {
+        }
+
+        public override double ComputeTax()
+        {
+            return Price * 0.1;
+        }
+    }
+
+}
